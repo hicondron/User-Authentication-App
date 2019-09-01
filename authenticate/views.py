dcc60961a0047f85b35281bf86f36daddda5dea8
Heather Condron <41925920+hicondron@users.noreply.github.com>
2019-09-01 07:57:58 -0400

    intial commit

---
+++ authenticate/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
+def home(request): 
+    return render(request, 'authenticate/home.html', {})
+
+def login_user(request):
+    if request.method == 'POST': 
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return redirect('login')
+    else:
+        return render(request, 'authenticate/login.html', {})
